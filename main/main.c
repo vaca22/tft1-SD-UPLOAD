7fdb6c3c5f864e51c279766c6b5becf57071a4b2
vaca <wsmslgh@gmail.com>
2022-06-23 04:20:08 -0700

    sadfrdsftfdtrdsfdfsesdfwtrrtj54erdfgejuhdyfgwdrs

--- main/main.c
+++ main/main.c
@@ -1,11 +1,3 @@
-/* SPI Master example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -366,6 +358,19 @@ static void ble_task(void *pvParameters) {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void app_main(void) {
     initialize_nvs();
