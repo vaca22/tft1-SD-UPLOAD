3a59ce99e724a9309e611e4ab7899014455fdd42
vaca <wsmslgh@gmail.com>
2022-06-24 19:17:49 -0700

    sdf5tfdgghgtrffdrgdfgsdffdgfdfertggfsa

--- main/main.c
+++ main/main.c
@@ -275,8 +275,8 @@ void wifi_init_sta(void) {
         ESP_ERROR_CHECK(esp_netif_init());
 
         ESP_ERROR_CHECK(esp_event_loop_create_default());
-        esp_netif_create_default_wifi_sta();
-
+        esp_netif_t* obj=esp_netif_create_default_wifi_sta();
+        esp_netif_set_hostname(obj, "lghGood");
         wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
         ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
